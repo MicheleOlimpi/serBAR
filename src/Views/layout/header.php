798d2470c5454d53a7b04faeec260717d4a9e27e
Michele Olimpi <michele.olimpi@gmail.com>
2026-03-14 00:56:53 +0100

    Corretto nome icona tipo giorno
    Corretto nome icona tipo giorno

--- src/Views/layout/header.php
+++ src/Views/layout/header.php
@@ -13,7 +13,7 @@ $adminNavItems = [
     'dashboard' => ['label' => 'Dashboard', 'href' => './', 'icon' => 'fa-gauge'],
     'boards' => ['label' => 'Tabelloni', 'href' => '?action=boards', 'icon' => 'fa-table'],
     'users' => ['label' => 'Utenti', 'href' => '?action=users', 'icon' => 'fa-user'],
-    'day_types' => ['label' => 'Tipi giorno', 'href' => '?action=day_types', 'icon' => 'calendar-day'],
+    'day_types' => ['label' => 'Tipi giorno', 'href' => '?action=day_types', 'icon' => 'fa-calendar-day'],
     'shift_config' => ['label' => 'Turni giornalieri', 'href' => '?action=shift_config', 'icon' => 'fa-clock-rotate-left'],
     'calendar' => ['label' => 'Calendario', 'href' => '?action=calendar', 'icon' => 'fa-calendar-days'],
     'notifications' => ['label' => 'Segnalazioni', 'href' => '?action=notifications', 'icon' => 'fa-message'],
