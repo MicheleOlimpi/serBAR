c60c86743d925d4b5a19ff509c0c2355f5be7be8
Michele Olimpi <michele.olimpi@gmail.com>
2026-03-31 01:39:56 +0200

    Remove serviceMode parameter

--- config/app.php
+++ config/app.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    'serviceMode' => false,           // Enable service mode 
     'host' => '127.0.0.1',            // DB hostanme or IP address
     'database' => 'servizioBAR',      // DB name
     'username' => 'root',             // DB username
