275bcfa48fb8259f87b9dc5cee17528a1b397cae
Michele Olimpi <michele.olimpi@gmail.com>
2026-05-24 14:31:33 +0200

    Change page title from 'ACLI servizio BAR' to 'serBAR'

--- src/Views/layout/header.php
+++ src/Views/layout/header.php
@@ -48,7 +48,7 @@ $consultationNavItems['information'] = ['label' => 'Informazioni', 'href' => '?a
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>ACLI servizio BAR</title>
+  <title>serBAR</title>
   <link rel="icon" type="image/svg+xml" href="<?= htmlspecialchars($assetBasePath . '/serBAR-square.svg') ?>">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" rel="stylesheet">
