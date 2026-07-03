199a1cb420646f0565bb94e4b70fa7a8b645dba0
Michele Olimpi <michele.olimpi@gmail.com>
2026-07-03 21:52:23 +0200

    Update dashboard.php

--- src/Views/consultation/dashboard.php
+++ src/Views/consultation/dashboard.php
@@ -227,7 +227,7 @@ $statusLabels = [
 </div>
 
 <div class="modal fade" id="notificationDetailModal" tabindex="-1" aria-labelledby="notificationDetailModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
+  <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="notificationDetailModalLabel">Segnalazione</h5>
